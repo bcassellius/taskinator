53b29f28826f49110a7656728219368aceed0f13
Bri <bhilliker@gmail.com>
2021-06-20 22:20:06 -0500

    add save edited task

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,20 +8,40 @@ var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    //   package up data as an object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-    };
+    var isEdit = formEl.hasAttribute("data-task-id");
+
     // check if input values are empty strings
     if (!taskNameInput || !taskTypeInput) {
         alert("You need to fill out the task form!");
         return false;
     }
-    
-    // send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
+        // has data attribute, so get task id and call function to complete edit process
+    if (isEdit){
+         var taskId = formEl.getAttribute("data-task-id");
+         completeEditTask(taskNameInput, taskTypeInput, taskId);
+     }
+    //  no data attribute, so create object as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+        createTaskEl(taskDataObj);
+    }
+
     formEl.reset();
+};
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    // find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    // set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!")
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
 };
 
 var createTaskEl = function(taskDataObj) {
